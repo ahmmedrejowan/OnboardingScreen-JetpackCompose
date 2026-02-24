944dd14dfccb3c5e2e3d00c1ff45a7db6fcad07f
K M Rejowan Ahmmed <kmrejowan@gmail.com>
2026-02-24 18:47:15 +0600

    Add routes for 16 new onboarding variations

--- app/src/main/java/com/rejowan/onboardingjc/navigation/Routes.kt
+++ app/src/main/java/com/rejowan/onboardingjc/navigation/Routes.kt
@@ -55,6 +55,54 @@ sealed interface Routes {
     @Serializable
     data object ZoomTransitionOnboarding : Routes
 
+    @Serializable
+    data object Carousel3DOnboarding : Routes
+
+    @Serializable
+    data object CardFlipOnboarding : Routes
+
+    @Serializable
+    data object CubeTransitionOnboarding : Routes
+
+    @Serializable
+    data object FadeCrossfadeOnboarding : Routes
+
+    @Serializable
+    data object GlassmorphismOnboarding : Routes
+
+    @Serializable
+    data object NeumorphismOnboarding : Routes
+
+    @Serializable
+    data object TypewriterOnboarding : Routes
+
+    @Serializable
+    data object SwipeCardsOnboarding : Routes
+
+    @Serializable
+    data object CircularRevealOnboarding : Routes
+
+    @Serializable
+    data object PulsatingOnboarding : Routes
+
+    @Serializable
+    data object ParticleOnboarding : Routes
+
+    @Serializable
+    data object LiquidBlobOnboarding : Routes
+
+    @Serializable
+    data object StorytellingOnboarding : Routes
+
+    @Serializable
+    data object InteractiveQuizOnboarding : Routes
+
+    @Serializable
+    data object ChatBubblesOnboarding : Routes
+
+    @Serializable
+    data object RotatingWheelOnboarding : Routes
+
     // Success screen
     @Serializable
     data class Success(val onboardingName: String) : Routes
